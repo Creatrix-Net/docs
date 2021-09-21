ec625ee8de7917cdc492b4c9bd1afae7811eeb7f
Dhruva Shaw <34207400+Dhruvacube@users.noreply.github.com>
2021-09-21 13:14:54 +0530

    automated commands and partners fully

--- commands_and_partners/__init__.py
+++ commands_and_partners/__init__.py
@@ -1,14 +1,31 @@
 import json
 
 from pathlib import Path
-import os, ast, requests
+import os, requests
 
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+try:
+    import dotenv
+    dotenv.load_dotenv(BASE_DIR/ os.path.join('config', '.env'))
+except:
+    pass
 
 class Partners:
     def __init__(self, json_dict: dict):
         for i in json_dict:
             setattr(self, i, json_dict[i])
+
+class Command:
+    def __init__(self, json_dict: dict):
+        for i in json_dict:
+            setattr(self, i, json_dict[i])
+            
+class CommandGroups:
+    def __init__(self, list_dict: list, name: str):
+        self.name = name.capitalize().strip(' ')
+        self.commands_list = [Command(list_dict[count]) for count,i in enumerate(list_dict)]
+
 
 def define_env(env):
     global BASE_DIR
@@ -23,6 +40,17 @@ def define_env(env):
         return [ add_data(i, data[i]) for i in data ]
     
     env.variables['partners'] = partners()
+    
+    def commands_groups():
+        req = requests.get(
+            'https://fateslist.xyz/api/v2/bots/779559821162315787/commands',
+            headers={'Authorization': os.environ.get('FATES_USER_TOKEN')}
+        )
+        commands_json = req.json()
+        return [CommandGroups(commands_json[i], i) for i in commands_json]
+    
+    env.variables['commands'] = commands_groups()
+    
 
     @env.filter
     def format_description(x):
