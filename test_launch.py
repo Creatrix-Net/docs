bfceb2f2eabc017b7a407ff83653de1311ff081d
Dhruva Shaw <me@dhruvashaw.me>
2025-01-08 00:07:14 +0530

    ads

--- test_launch.py
+++ test_launch.py
@@ -5,6 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent
 try:
     import dotenv
     dotenv.load_dotenv(BASE_DIR/ os.path.join('config', '.env'))
+    os.environ['path'] += r';C:\Program Files\UniConvertor-2.0rc5\dlls'
 except:
     pass
 
